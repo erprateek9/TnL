b651c5fcae4ae9af675d830e5d0ddaabde22b0d1
Prateek Yadav <erprateek9@gmail.com>
2023-09-03 22:42:04 +0530

    added selection sort

---
+++ java/DSA_with_java/Sorting/SSV1Runner.java
@@ -0,0 +1,24 @@
+package DSA_with_java.Sorting;
+import java.util.Scanner;
+public class SSV1Runner {
+    public static void main(String[] args) {
+        System.out.println("Enter the size of array");
+        Scanner l= new Scanner(System.in);
+        int len = l.nextInt();
+        SelectionSortV1 array= new SelectionSortV1(len);
+        array.initializer();
+        array.printer();
+        // choice for sorting
+
+        System.out.println("for Ascending order,Press 1");
+        System.out.println("for Decending order,Press 2");
+        Scanner ch= new Scanner(System.in);
+        int choice = l.nextInt();
+        if (choice==1)array.ascender();
+        else if (choice==2) array.decender();
+        else  System.out.println("INVALID CHOICE");
+        array.printer();
+ 
+ 
+    }
+}
---
+++ java/DSA_with_java/Sorting/SelectionSortV1.java
@@ -0,0 +1,64 @@
+package DSA_with_java.Sorting;
+import java.util.Random;
+public class SelectionSortV1 {
+    int arr[];
+    int len;
+    SelectionSortV1(int l){
+        len=l;
+        arr= new int[len];
+    }
+    void initializer(){
+        System.out.println("Generating random values...");
+        Random rand= new Random();
+        for (int i =0;i<arr.length;i++){
+            arr[i]= rand.nextInt(1000);
+        }
+
+    }
+    void printer(){
+        for (int i =0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        } 
+        System.out.println("");
+    }
+    void ascender(){
+        int smallest,temp;
+        
+        smallest=arr[0];
+        for (int i= 0;i<arr.length;i++){
+            smallest=i;
+            for(int j=i+1;j<arr.length;j++){
+                
+                if (arr[smallest]>arr[j]){
+                    smallest=j;
+                }
+            }
+            temp=arr[i];
+            arr[i]=arr[smallest];
+            arr[smallest]=temp;
+
+            
+        
+        }
+
+    }
+    void decender(){
+        int largest,temp;
+        
+        largest=arr[0];
+        for (int i= 0;i<arr.length;i++){
+            largest=i;
+            for(int j=i+1;j < arr.length;j++){
+                
+                if (arr[largest]<arr[j]){
+                    largest=j;
+                }
+            }
+            temp=arr[i];
+            arr[i]=arr[largest];
+            arr[largest]=temp;
+        }
+    }
+
+
+}
