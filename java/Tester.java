25b01b6faec02209647d7bfea75d1174161cbc41
Prateek Yadav <erprateek9@gmail.com>
2023-11-29 15:25:18 +0530

    t

--- java/Tester.java
+++ java/Tester.java
@@ -2,8 +2,18 @@ class Tester {
     
    public static int[] findLeapYears(int year){
        //Implement your code here and change the return value accordingly
-	   
-        return null;
+	   int count=1,int i=0;
+	   int leapYrs[]= new int[15];
+	   while(count<=15){
+			if((year%400==0)||(year%4==0&&year%100!=0)){
+				leapYrs[i]=year;
+				i++;
+				year++;
+				count++;
+			}
+			else {year++;}
+	   }
+        return leapYrs;
    }
     
    public static void main(String[] args) {
